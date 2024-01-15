3370bf82ec0bf8f161f8f309cad8c8fbe5212875
Mykyta Zhuravlov <zhuravlov.mykyta@gmail.com>
2024-01-15 21:45:58 +0200

    lesson02 added

---
+++ lesson02_cvs_codequality_dependencies_cicd/oop.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+
+class Price:
+    def __init__(self, currency, value) -> None:
+        self.currency = currency
+        self.value = value
+
+    # TODO: complete this class
+
+# phone = 1000 USD
+# food = 10 EUR
+# total = phone + food
+
+# 1000 + (100/39) = 1000 + 2.56410256 USD
+
+
+a = 10
+b = 20
+c = a + b
