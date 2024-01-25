47d5db653b40d241cb9250a58e2e4e0e3be11490
Mykyta Zhuravlov <zhuravlov.mykyta@gmail.com>
2024-01-25 22:57:30 +0200

    lesson05 added

---
+++ lesson05_mro/args_kwargs.py
@@ -0,0 +1,9 @@
+def create_product(*names: str | int, **kwargs):
+    print(names)
+    print(kwargs)
+
+
+create_product("table", 20, 30, price=222)
+#create_product(name="table", price=20)
+#create_product("table", price=20)
+#create_product(name="table", 20) - error
---
+++ lesson05_mro/initializer.py
@@ -0,0 +1,15 @@
+class Product:
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        print(f"from new: {instance}")
+        return instance
+
+    def __init__(self, name: str, price: int) -> None:
+        print(f"from init {self}")
+        self.name: str = name
+        self.price: int = price
+
+
+print(Product.__mro__)
+phone = Product(name="Table", price=20000)
+print(phone)
